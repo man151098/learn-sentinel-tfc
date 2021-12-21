ef80a1195ca4bdf47314e66b8ac4303e95f1c67f
man151098 <56228310+man151098@users.noreply.github.com>
2021-12-21 15:16:16 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "coursez"
+    organization = "manjiree"
 
     workspaces {
       name = "sentinel-example"
