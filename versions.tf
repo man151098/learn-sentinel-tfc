888efc56a21d13c566b90ee2cef5511b3d7903e1
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2021-12-21 12:38:31 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "coursez"
 
     workspaces {
       name = "sentinel-example"
